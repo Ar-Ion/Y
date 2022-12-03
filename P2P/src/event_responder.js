9897eb5d645e81852bf04f31783fa4c25fd6332a
Arion <arion@innovazion.ch>
2022-12-03 18:22:30 +0100

    IPFS backend and electron frontend

---
+++ P2P/src/event_responder.js
@@ -0,0 +1,5 @@
+const { app, BrowserWindow } = require('electron')
+
+document.querySelector('#sign_artwork').addEventListener('click', () => {
+    
+})
---
+++ P2P/src/main.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile("src/dashboard.html");
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", async () => {
+  createWindow();
+
+  try {
+    const IPFS = await import('ipfs-core')
+    const node = await IPFS.create();
+    const id = await node.id();
+    console.log(id);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
