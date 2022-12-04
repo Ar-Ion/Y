9555781d89847afe1f8445b056fcd30a734d5748
Arion <arion@innovazion.ch>
2022-12-04 05:22:59 +0100

    Connected front-end to back-end

--- P2P/src/main.js
+++ P2P/src/main.js
@@ -1,13 +1,29 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog, ipcMain } = require('electron')
+
+const path = require('path')
+const fs = require('fs')
 
 let mainWindow;
+let selectedFile;
+let globalNode;
+let globalPeerID;
+
+userRegistrySynced = false;
+let userRegistry = {};
+userRegistry.user = {};
+userRegistry.user.following = [];
+userRegistry.artist = {};
+userRegistry.artist.content = [];
+userRegistry.artist.followers = [];
+userRegistry.lastUpdate = 0;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'preload.js')
     }
   });
 
@@ -18,16 +34,184 @@ function createWindow() {
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+
+  mainWindow.webContents.openDevTools()
+}
+
+ipcMain.on('select_artwork', (event) => {
+  // If the platform is 'win32' or 'Linux'
+  if (process.platform !== 'darwin') {
+    dialog.showOpenDialog({
+      title: 'Select the File to be uploaded',
+      defaultPath: path.join(__dirname, '../assets/'),
+      buttonLabel: 'Upload',
+      properties: ['openFile']
+    }).then(file => {
+        selectedFile = null;
+      if (!file.canceled) {
+        const filepath = file.filePaths[0].toString();
+        selectedFile = filepath;
+        event.reply('artwork_selected', path.parse(filepath).base)
+      }
+    }).catch(err => {
+        selectedFile = null;
+      console.log(err)
+    });
+  }
+  else {
+    // If the platform is 'darwin' (macOS)
+    dialog.showOpenDialog({
+      title: 'Select the File to be uploaded',
+      defaultPath: path.join(__dirname, '../assets/'),
+      buttonLabel: 'Upload',
+      properties: ['openFile']
+    }).then(file => {
+        selectedFile = null;
+      if (!file.canceled) {
+          const filepath = file.filePaths[0].toString();
+          selectedFile = filepath;
+          event.reply('artwork_selected', path.parse(filepath).base)
+        }
+  }).catch(err => {
+      selectedFile = null;
+      console.log(err)
+    });
+}
+});
+
+ipcMain.on('load_registry', (event) => {
+    event.reply('registry_loaded', userRegistrySynced, userRegistry);
+});
+
+
+async function update_user_registry() {
+    userRegistry.lastUpdate = Date.now();
+
+    try {
+        fs.writeFileSync('user_registry.db', JSON.stringify(userRegistry), 'utf-8');
+    } catch(e) {
+        console.log(e)
+    }
+
+    const resultRegistry = await globalNode.add(JSON.stringify(userRegistry));
+
+    const results = await globalNode.name.publish(resultRegistry.path);
+    console.log(results)
+}
+
+async function sign_artwork(payload) {
+    if(selectedFile != null) {
+        console.log("Signing " + selectedFile + "...")
+
+        const buffer_content = fs.readFileSync(selectedFile);
+        const result_content = await globalNode.add(buffer_content);
+
+        var date = new Date
+        const result = date.toLocaleString("en-GB", {timeZone: 'CET'})
+
+        payload.date_created = result;
+        payload.date_updated = result;
+        payload.signature = result_content.path;
+
+        userRegistry.artist.content.push(payload)
+
+        await update_user_registry()
+
+        selectedFile = null;
+        mainWindow.webContents.send('artwork_signed', payload)
+    }
+}
+
+ipcMain.on('sign_artwork', (event, payload) => {
+    sign_artwork(payload)
+})
+
+
+async function all(source) {
+  const arr = []
+
+  for await (const entry of source) {
+    arr.push(entry)
+  }
+
+  return arr
+}
+
+function asUint8Array (buf) {
+  if (globalThis.Buffer != null) {
+    return new Uint8Array(buf.buffer, buf.byteOffset, buf.byteLength)
+  }
+
+  return buf
+}
+
+function concat (arrays, length) {
+  if (length == null) {
+    length = arrays.reduce((acc, curr) => acc + curr.length, 0)
+  }
+
+  const output = Buffer.allocUnsafe(length)
+  let offset = 0
+
+  for (const arr of arrays) {
+    output.set(arr, offset)
+    offset += arr.length
+  }
+
+  return asUint8Array(output)
+}
+
+function toString (array) {
+    return globalThis.Buffer.from(array.buffer, array.byteOffset, array.byteLength).toString('utf8')
+}
+
+
+
+async function fetchRegistry(ipfsPath) {
+    const data = concat(await all(globalNode.cat(ipfsPath)))
+    const serializedRegistry = toString(data)
+    return JSON.parse(serializedRegistry)
 }
 
 app.on("ready", async () => {
   createWindow();
 
   try {
+      userRegistry = JSON.parse(fs.readFileSync('user_registry.db', 'utf-8'));
+  } catch(e) {
+      console.log(e)
+  }
+
+  try {
+
     const IPFS = await import('ipfs-core')
     const node = await IPFS.create();
     const id = await node.id();
-    console.log(id);
+
+    globalNode = node;
+    globalPeerID = id;
+
+    var mostRecentTime = 0;
+    var mostRecentRegistry;
+
+    for await (const name of node.name.resolve(id.id)) {
+      console.log(name)
+      candidate = await fetchRegistry(name)
+
+      console.log(candidate)
+
+      if(candidate.lastUpdate > mostRecentTime) {
+          mostRecentTime = candidate.lastUpdate;
+          mostRecentRegistry = candidate;
+      }
+
+      userRegistry = mostRecentRegistry;
+      userRegistrySynced = true;
+
+      mainWindow.webContents.send('registry_loaded', userRegistrySynced, userRegistry);
+    }
+
+    console.log("Latest registry: " + mostRecentTime)
   } catch (err) {
     console.error(err);
   }
---
+++ P2P/src/preload.js
@@ -0,0 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    select_artwork: () => ipcRenderer.send('select_artwork'),
+    sign_artwork: (payload) => ipcRenderer.send('sign_artwork', payload),
+    load_registry: () => ipcRenderer.send('load_registry'),
+    artwork_selected: (func) => {
+        ipcRenderer.on('artwork_selected', (event, artwork) => func(artwork));
+    },
+    artwork_signed: (func) => {
+        ipcRenderer.on('artwork_signed', (event, payload) => func(payload));
+    },
+    registry_loaded: (func) => {
+        ipcRenderer.on('registry_loaded', (event, synced, userRegistry) => func(synced, userRegistry));
+    }
+})
